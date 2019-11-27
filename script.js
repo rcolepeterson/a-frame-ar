6e34751aba74ebc4580edc6b4890363420a84d1f
John Walters <john.walters@possible.com>
2019-11-26 17:30:43 -0800

    Added modals for aframe items on screen.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 window.onload = () => {
   
+
+    document.querySelector('a-scene').addEventListener('loaded', function () {
+
+        document.getElementById("cokeg1").addEventListener("click", ShowIconContent);
+        document.getElementById("cokeg2").addEventListener("click", ShowIconContent);
+        document.getElementById("cokeg3").addEventListener("click", ShowIconContent);
         
+    })
+
+   
 
     
   };
